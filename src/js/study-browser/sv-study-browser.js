92335c19289cb713ec3d590b7b7709937c33fb32
kandaj <kandaj@ebi.ac.uk>
2015-08-06 10:45:59 +0100

    EVA License updated

--- src/js/study-browser/sv-study-browser.js
+++ src/js/study-browser/sv-study-browser.js
@@ -15,7 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *//
+ */
 
 function SvStudyBrowser(args) {
     _.extend(this, Backbone.Events);
