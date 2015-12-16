6887ad04fa2b6def6d67901c20d8f7d844fdcc7a
kandaj <kandaj@ebi.ac.uk>
2015-12-16 12:20:57 +0000

    MAF filter tooltip added

--- src/js/variant-widget/filters/eva-population-frequency-filter-form-panel.js
+++ src/js/variant-widget/filters/eva-population-frequency-filter-form-panel.js
@@ -136,7 +136,7 @@ EvaPopulationFrequencyFilterFormPanel.prototype = {
             defaultType: 'textfield',
             items: [
                 {
-                    fieldLabel: '<span class="title-header-icon" data-qtip="Minor Allele Frequency" style="margin-bottom:0px;">MAF</span>',
+                    fieldLabel: '<img class="text-header-icon" data-qtip="Minor Allele Frequency." style="margin-bottom:0px;" src="img/icon-info.png"/>&nbsp;<span class="title-header-icon" data-qtip="Minor Allele Frequency" style="margin-bottom:0px;">MAF</span>',
                     name: 'maf',
                     width: '100%',
                     margin: '5 0 5 0',
