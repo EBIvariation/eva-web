d66aadb8691bac56f1d0f1e58a0e93aa00be918b
kandaj <kandaj@ebi.ac.uk>
2016-02-11 14:24:40 +0000

    grunt file updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -310,7 +310,7 @@ module.exports = function (grunt) {
     grunt.registerTask('test',['mochaTest']);
 
     // Default task.
-    grunt.registerTask('default', ['hub:genomeViewer','clean:eva','concat','uglify', 'copy:eva','cssmin', 'htmlbuild:eva', 'imagemin'])
+    grunt.registerTask('default', ['hub:genomeViewer','clean:eva','concat','uglify', 'copy:eva','cssmin', 'htmlbuild:eva', 'minifyHtml', 'imagemin'])
 
 
 
