95e348354672b326e62a4bdd2fc6879c36ee09fd
Jag <jagi.work@gmail.com>
2016-07-04 10:13:15 +0100

    consequence type filter sorted alphabetically (#9)
    * consequence type filter sort order updated

--- src/js/eva-config.js
+++ src/js/eva-config.js
@@ -131,23 +131,21 @@ consequenceTypes = [
                 expanded: true,
                 checked: false,
                 children: [
+                    {acc: 'SO:0001580', name: 'coding_sequence_variant', qtip: 'A sequence variant that changes the coding sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#458B00' },
+                    {acc: 'SO:0001907', name: 'feature_elongation', qtip: 'A sequence variant that causes the extension of a genomic feature, with regard to the reference sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#7F7F7F' },
+                    {acc: 'SO:0001906', name: 'feature_truncation', qtip: 'A sequence variant that causes the reduction of a genomic feature, with regard to the reference sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#7F7F7F' },
+                    {acc: 'SO:0001589', name: 'frameshift_variant', qtip: 'A sequence variant which causes a disruption of the translational reading frame, because the number of nucleotides inserted or deleted is not a multiple of three', leaf: true, checked: false, iconCls: 'no-icon', color: '#9400D3' },
+                    {acc: 'SO:0001626', name: 'incomplete_terminal_codon_variant', qtip: 'A sequence variant where at least one base of the final codon of an incompletely annotated transcript is changed', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF00FF'},
+                    {acc: 'SO:0001822', name: 'inframe_deletion', qtip: 'An inframe non synonymous variant that deletes bases from the coding sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF69B4'},
+                    {acc: 'SO:0001821', name: 'inframe_insertion', qtip: 'An inframe non synonymous variant that inserts bases into in the coding sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF69B4'},
+                    {acc: 'SO:0001583', name: 'missense_variant', qtip: 'A sequence variant, that changes one or more bases, resulting in a different amino acid sequence but where the length is preserved', leaf: true, checked: false, iconCls: 'no-icon', color: '#FFD700' },
+                    {acc: 'SO:0001621', name: 'NMD_transcript_variant', qtip: 'A variant in a transcript that is the target of NMD', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF4500' },
+                    {acc: 'SO:0001818', name: 'protein_altering_variant', qtip: 'A sequence_variant which is predicted to change the protein encoded in the coding sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF0080' },
+                    {acc: 'SO:0001819', name: 'synonymous_variant', qtip: 'A sequence variant where there is no resulting change to the encoded amino acid', leaf: true, checked: false, iconCls: 'no-icon', color: '#76EE00' },
                     {acc: 'SO:0002012', name: 'start_lost', qtip: 'A codon variant that changes at least one base of the canonical start codon', leaf: true, checked: false, iconCls: 'no-icon', color: '#FFD700' },
                     {acc: 'SO:0001587', name: 'stop_gained', qtip: 'A sequence variant whereby at least one base of a codon is changed, resulting in a premature stop codon, leading to a shortened transcript', leaf: true, checked: false, iconCls: 'no-icon', color: '#ff0000' },
                     {acc: 'SO:0001578', name: 'stop_lost', qtip: 'A sequence variant where at least one base of the terminator codon (stop) is changed, resulting in an elongated transcript', leaf: true, checked: false, iconCls: 'no-icon', color: '#ff0000' },
-                    {acc: 'SO:0001818', name: 'protein_altering_variant', qtip: 'A sequence_variant which is predicted to change the protein encoded in the coding sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF0080' },
-                    {acc: 'SO:0001821', name: 'inframe_insertion', qtip: 'An inframe non synonymous variant that inserts bases into in the coding sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF69B4'},
-                    {acc: 'SO:0001822', name: 'inframe_deletion', qtip: 'An inframe non synonymous variant that deletes bases from the coding sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF69B4'},
-                    {acc: 'SO:0001589', name: 'frameshift_variant', qtip: 'A sequence variant which causes a disruption of the translational reading frame, because the number of nucleotides inserted or deleted is not a multiple of three', leaf: true, checked: false, iconCls: 'no-icon', color: '#9400D3' },
-                    {acc: 'SO:0001621', name: 'NMD_transcript_variant', qtip: 'A variant in a transcript that is the target of NMD', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF4500' },
-//                {acc: 'SO:0001582', name: 'initiator_codon_variant', qtip: 'A codon variant that changes at least one base of the first codon of a transcript',leaf: true,checked: false,  iconCls :'no-icon',color:'#FF4500' },
-                    {acc: 'SO:0001626', name: 'incomplete_terminal_codon_variant', qtip: 'A sequence variant where at least one base of the final codon of an incompletely annotated transcript is changed', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF00FF'},
-                    {acc: 'SO:0001583', name: 'missense_variant', qtip: 'A sequence variant, that changes one or more bases, resulting in a different amino acid sequence but where the length is preserved', leaf: true, checked: false, iconCls: 'no-icon', color: '#FFD700' },
-                    {acc: 'SO:0001819', name: 'synonymous_variant', qtip: 'A sequence variant where there is no resulting change to the encoded amino acid', leaf: true, checked: false, iconCls: 'no-icon', color: '#76EE00' },
-                    {acc: 'SO:0001567', name: 'stop_retained_variant', qtip: 'A sequence variant where at least one base in the terminator codon is changed, but the terminator remains', leaf: true, checked: false, iconCls: 'no-icon', color: '#76EE00' },
-                    {acc: 'SO:0001580', name: 'coding_sequence_variant', qtip: 'A sequence variant that changes the coding sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#458B00' },
-                    {acc: 'SO:0001907', name: 'feature_elongation', qtip: 'A sequence variant that causes the extension of a genomic feature, with regard to the reference sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#7F7F7F' },
-                    {acc: 'SO:0001906', name: 'feature_truncation', qtip: 'A sequence variant that causes the reduction of a genomic feature, with regard to the reference sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#7F7F7F' }
-
+                    {acc: 'SO:0001567', name: 'stop_retained_variant', qtip: 'A sequence variant where at least one base in the terminator codon is changed, but the terminator remains', leaf: true, checked: false, iconCls: 'no-icon', color: '#76EE00' }
                 ]
             },
             {
@@ -159,11 +157,12 @@ consequenceTypes = [
                 expanded: true,
                 checked: false,
                 children: [
-                    {acc: 'SO:0001623', name: '5_prime_UTR_variant', qtip: 'A UTR variant of the 5\' UTR', leaf: true, checked: false, iconCls: 'no-icon', color: '#7AC5CD'},
                     {acc: 'SO:0001624', name: '3_prime_UTR_variant', qtip: 'A UTR variant of the 3\' UTR', leaf: true, checked: false, iconCls: 'no-icon', color: '#7AC5CD'},
+                    {acc: 'SO:0001623', name: '5_prime_UTR_variant', qtip: 'A UTR variant of the 5\' UTR', leaf: true, checked: false, iconCls: 'no-icon', color: '#7AC5CD'},
+                    {acc: 'SO:0001627', name: 'intron_variant', qtip: 'A transcript variant occurring within an intron', leaf: true, checked: false, iconCls: 'no-icon', color: '#02599C' },
                     {acc: 'SO:0001792', name: 'non_coding_transcript_exon_variant', qtip: 'A sequence variant that changes non-coding exon sequence', leaf: true, checked: false, iconCls: 'no-icon', color: '#32CD32' },
 //                    {acc: 'SO:0001619', name: 'nc_transcript_variant', qtip: 'A transcript variant of a non coding RNA',leaf: true,checked: false,  iconCls :'no-icon' },
-                    {acc: 'SO:0001627', name: 'intron_variant', qtip: 'A transcript variant occurring within an intron', leaf: true, checked: false, iconCls: 'no-icon', color: '#02599C' }
+
                 ]
 
             },
@@ -176,8 +175,8 @@ consequenceTypes = [
                 expanded: true,
                 checked: false,
                 children: [
-                    {acc: 'SO:0001575', name: 'splice_donor_variant', qtip: 'A splice variant that changes the 2 base region at the 5\' end of an intron', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF581A'},
                     {acc: 'SO:0001574', name: 'splice_acceptor_variant', qtip: 'A splice variant that changes the 2 base region at the 3\' end of an intron', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF581A'},
+                    {acc: 'SO:0001575', name: 'splice_donor_variant', qtip: 'A splice variant that changes the 2 base region at the 5\' end of an intron', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF581A'},
                     {acc: 'SO:0001630', name: 'splice_region_variant', qtip: 'A sequence variant in which a change has occurred within the region of the splice site, either within 1-3 bases of the exon or 3-8 bases of the intron', leaf: true, checked: false, iconCls: 'no-icon', color: '#FF7F50' }
 
                 ]
@@ -198,13 +197,13 @@ consequenceTypes = [
         checked: false,
         iconCls: 'no-icon',
         children: [
+            {acc: 'SO:0001620', name: 'mature_miRNA_variant', qtip: 'A transcript variant located with the sequence of the mature miRNA', leaf: true, checked: false, iconCls: 'no-icon', color: '#458B00'},
+            {acc: 'SO:0001894', name: 'regulatory_region_ablation', qtip: 'A feature ablation whereby the deleted region includes a regulatory region', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' },
+            {acc: 'SO:0001891', name: 'regulatory_region_amplification', qtip: 'A feature amplification of a region containing a regulatory region', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' },
+            {acc: 'SO:0001566', name: 'regulatory_region_variant', qtip: 'A sequence variant located within a regulatory region', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' },
             {acc: 'SO:0001782', name: 'TF_binding_site_variant', qtip: 'A sequence variant located within a transcription factor binding site', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' },
             {acc: 'SO:0001895', name: 'TFBS_ablation', qtip: 'A feature ablation whereby the deleted region includes a transcription factor binding site', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' },
-            {acc: 'SO:0001892', name: 'TFBS_amplification', qtip: 'A feature amplification of a region containing a transcription factor binding site', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' },
-            {acc: 'SO:0001620', name: 'mature_miRNA_variant', qtip: 'A transcript variant located with the sequence of the mature miRNA', leaf: true, checked: false, iconCls: 'no-icon', color: '#458B00'},
-            {acc: 'SO:0001566', name: 'regulatory_region_variant', qtip: 'A sequence variant located within a regulatory region', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' },
-            {acc: 'SO:0001894', name: 'regulatory_region_ablation', qtip: 'A feature ablation whereby the deleted region includes a regulatory region', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' },
-            {acc: 'SO:0001891', name: 'regulatory_region_amplification', qtip: 'A feature amplification of a region containing a regulatory region', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' }
+            {acc: 'SO:0001892', name: 'TFBS_amplification', qtip: 'A feature amplification of a region containing a transcription factor binding site', leaf: true, checked: false, iconCls: 'no-icon', color: '#A52A2A' }
         ]
     },
     {
@@ -216,9 +215,10 @@ consequenceTypes = [
         iconCls: 'no-icon',
         children: [
             {acc: 'SO:0001636', name: '2KB_upstream_gene_variant', qtip: 'A sequence variant located within 2KB 5\' of a gene.', leaf: true, checked: false, iconCls: 'no-icon', color: '#A2B5CD'},
-            {acc: 'SO:0001631', name: 'upstream_gene_variant', qtip: 'A sequence variant located 5\' of a gene', leaf: true, checked: false, iconCls: 'no-icon', color: '#A2B5CD'},
             {acc: 'SO:0001632', name: 'downstream_gene_variant', qtip: 'A sequence variant located 3\' of a gene', leaf: true, checked: false, iconCls: 'no-icon', color: '#A2B5CD' },
-            {acc: 'SO:0001628', name: 'intergenic_variant', qtip: 'A sequence variant located in the intergenic region, between genes', leaf: true, checked: false, iconCls: 'no-icon', color: '#636363' }
+            {acc: 'SO:0001628', name: 'intergenic_variant', qtip: 'A sequence variant located in the intergenic region, between genes', leaf: true, checked: false, iconCls: 'no-icon', color: '#636363' },
+            {acc: 'SO:0001631', name: 'upstream_gene_variant', qtip: 'A sequence variant located 5\' of a gene', leaf: true, checked: false, iconCls: 'no-icon', color: '#A2B5CD'},
+
         ]
     }
 
