f63bffe77785298f2a675af2bcd8fef1f0f85bb2
kandaj <kandaj@ebi.ac.uk>
2016-07-21 11:19:17 +0100

    release verison chnaged to 2.1.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
         // Metadata.
         meta: {
             version: {
-                eva: '2.1.0'
+                eva: '2.1.1'
             }
         },
 
