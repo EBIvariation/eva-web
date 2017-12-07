395e7ada96d50ac39d4ef1712fc0e6e12bb0a2b6
jose miguel mut <jomutlo@gmail.com>
2017-12-07 14:41:04 +0000

    bump version to 3.8.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
         // Metadata.
         meta: {
             version: {
-                eva: '3.8.2'
+                eva: '3.8.3'
             }
         },
         serve: {
