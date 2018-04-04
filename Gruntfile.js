e744c10edc1b653fd2d4566f313ed112dac162ef
jose miguel mut <jomutlo@gmail.com>
2018-04-04 15:36:32 +0100

    bump version to 3.10.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
         // Metadata.
         meta: {
             version: {
-                eva: '3.10.1'
+                eva: '3.10.2'
             }
         },
         build: {
