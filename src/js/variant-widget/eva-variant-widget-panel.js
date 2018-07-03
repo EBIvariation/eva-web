c915d03179c5a2a40b914490de40da85f26cfda3
Pablo Arce Garcia <pablo.arce.garcia@gmail.com>
2018-07-03 13:59:59 +0100

    Removed unnecessary open comment symbols

--- src/js/variant-widget/eva-variant-widget-panel.js
+++ src/js/variant-widget/eva-variant-widget-panel.js
@@ -418,7 +418,7 @@ EvaVariantWidgetPanel.prototype = {
                         e.values.gene = e.values.gene.toUpperCase();
                     }
 
-                    if (typeof e.values.snp !== 'undefined') {//
+                    if (typeof e.values.snp !== 'undefined') {
                         e.values.snp = e.values.snp.replace(/\s+/g, ",");
                         e.values.snp = e.values.snp.replace(/,+/g, ",");
                         e.values.id = e.values.snp;
@@ -445,7 +445,7 @@ EvaVariantWidgetPanel.prototype = {
                         query = e.values.gene;
                     }
 
-//                    //<!--------Query by ID ----->
+                    //<!--------Query by ID ----->
                     if (e.values.id) {
                         resource = null;
                         category = 'variants';
