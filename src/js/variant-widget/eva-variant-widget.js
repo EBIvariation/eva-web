72e102a12edfee3fb99cd6df62d99cc37c293f5c
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2018-08-06 17:07:39 +0100

    Added link to Ensembl variant view

--- src/js/variant-widget/eva-variant-widget.js
+++ src/js/variant-widget/eva-variant-widget.js
@@ -362,10 +362,16 @@ EvaVariantWidget.prototype = {
                     id: 'variant-grid-view-column',
                     xtype: 'templatecolumn',
                     tpl: new Ext.XTemplate('<a href="?variant={chromosome}:{start}:{reference:htmlEncode}:{alternate:htmlEncode}&species={[this.getSpecies(values)]}&annotationVersion={[this.getAnnotationVersion()]}" target="_blank" class="image-link"><img class="eva-grid-img-active" src="img/eva_logo.png"/></a>' +
-                        '&nbsp;<tpl if="this.getURL(values)"><a href="{[this.getURL(values)]}" class="image-link dbsnp_link" target="_blank" onclick="ga(\'send\', \'event\', { eventCategory: \'Variant Browser\', eventAction: \'dbSNP Link\', eventLabel:this})"><span>dbSNP</span></a>' +
-                        '<tpl else><span  style="opacity:0.2" class="eva-grid-img-inactive ">dbSNP</span></tpl>',
+                        '&nbsp;<tpl if="this.getEnsemblURL(values)"><a href="{[this.getEnsemblURL(values)]}" class="image-link ensembl_link" target="_blank" onclick="ga(\'send\', \'event\', { eventCategory: \'Variant Browser\', eventAction: \'Ensembl Link\', eventLabel:this})"><img class="eva-grid-img-active" src="img/ensembl_logo.png"/></a>' +
+                        '<tpl else><img style="opacity:0.2" class="eva-grid-img-inactive src="img/ensembl_logo.png"/></span></tpl>' +
+                        '&nbsp;<tpl if="this.getDbsnpURL(values)"><a href="{[this.getDbsnpURL(values)]}" class="image-link dbsnp_link" target="_blank" onclick="ga(\'send\', \'event\', { eventCategory: \'Variant Browser\', eventAction: \'dbSNP Link\', eventLabel:this})"><span>dbSNP</span></a>' +
+                        '<tpl else><span style="opacity:0.2" class="eva-grid-img-inactive">dbSNP</span></tpl>',
                         {
-                            getURL: function (value) {
+                            getEnsemblURL: function (value) {
+                                var values = _this.getVariantId(value.ids);
+                                return values.ensemblURL;
+                            },
+                            getDbsnpURL: function (value) {
                                 var values = _this.getVariantId(value.ids);
                                 return values.dbsnpURL;
                             },
@@ -1020,6 +1026,7 @@ EvaVariantWidget.prototype = {
     },
     getVariantId: function (value) {
         var id = value;
+        var ensemblURL = '';
         var dbsnpURL = '';
         var rsRegEx = /^rs\d+$/;
         var ssRegEx = /^ss\d+$/;
@@ -1055,17 +1062,21 @@ EvaVariantWidget.prototype = {
         }
 
         if (id.match (rsRegEx)) {
+            ensemblURL = 'http://www.ensembl.org/Homo_sapiens/Variation/Explore?v=' + id;
             dbsnpURL = 'http://www.ncbi.nlm.nih.gov/SNP/snp_ref.cgi?rs=' + id;
         } else if (id.match (ssRegEx)) {
+            ensemblURL = 'http://www.ensembl.org/Homo_sapiens/Variation/Explore?v=' + id;
             dbsnpURL = 'http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ss.cgi?subsnp_id=' + id.substring (2);
         } else {
+            ensemblURL = false;
             dbsnpURL = false;
         }
 
-        var values = {variantId: id, dbsnpURL: dbsnpURL};
+        var values = {variantId: id, ensemblURL: ensemblURL, dbsnpURL: dbsnpURL};
 
         return values;
     },
+
     loadBottomPanel : function (panel, variant){
         var _this = this;
         if(!variant.reference){
