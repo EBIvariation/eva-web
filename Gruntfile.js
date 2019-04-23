b259f245b25b2b80c150b87e55bc57066f1c05d9
jose miguel mut <jomutlo@gmail.com>
2019-04-23 11:01:07 +0100

    bump version to 4.4.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
         // Metadata.
         meta: {
             version: {
-                eva: '4.3.0'
+                eva: '4.4.0'
             },
             submissionTemplate: {
                 version: 'V1.1.2'
