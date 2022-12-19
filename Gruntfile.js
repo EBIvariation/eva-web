efe8015bccaf97c4ec7e8990fd927739c45add8d
April Shen <ashen@ebi.ac.uk>
2022-12-19 10:07:27 +0000

    bump version

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
         // Metadata.
         meta: {
             version: {
-                eva: '4.4.7'
+                eva: '4.4.8'
             },
             submissionTemplate: {
                 version: 'V1.1.4'
