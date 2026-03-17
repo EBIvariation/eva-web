ea53177db7e8f48f1f7363d312406e3f33d7150e
nkumar2 <nkumar2@ebi.ac.uk>
2026-03-17 13:59:16 +0000

    update privacy notice in footer

--- src/js/footer/footer.js
+++ src/js/footer/footer.js
@@ -43,7 +43,7 @@
     init();
 })();
 (function includeScripts() {
-    var requireScripts = ['//www.ebi.ac.uk/web_guidelines/EBI-Framework/v1.1/js/cookiebanner.js', '//www.ebi.ac.uk/web_guidelines/EBI-Framework/v1.1/js/script.js', '//www.ebi.ac.uk/web_guidelines/js/downtime.js?' + Math.round(new Date().getTime() / 3600000)];
+    var requireScripts = ['//www.ebi.ac.uk/web_guidelines/EBI-Framework/v1.1/js/script.js', '//www.ebi.ac.uk/web_guidelines/js/downtime.js?' + Math.round(new Date().getTime() / 3600000)];
 
     function init() {
         try {
