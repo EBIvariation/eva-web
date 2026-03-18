f344f9b8d75987d1706cb3eee9e0525dfa18bbae
nkumar2 <nkumar2@ebi.ac.uk>
2026-03-18 11:07:38 +0000

    Bumping version to 4.4.26

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
         // Metadata.
         meta: {
             version: {
-                eva: '4.4.25'
+                eva: '4.4.26'
             },
             submissionTemplate: {
                 version: 'V1.1.5'
