6e06ad461ea0e0f7c249543082b03ebfb9e2a6d9
nkumar2 <nkumar2@ebi.ac.uk>
2026-09-17 14:22:44 +0100

    Advertise EVA workshop 2026

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
         // Metadata.
         meta: {
             version: {
-                eva: '4.4.28'
+                eva: '4.4.29'
             },
             submissionTemplate: {
                 version: 'V1.1.5'
