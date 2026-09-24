54ee8fdbff45a30dd7c89f2329d264534d9943f9
tcezard <tcezard@ebi.ac.uk>
2026-09-24 17:33:50 +0100

    prepare release 4.5.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
         // Metadata.
         meta: {
             version: {
-                eva: '4.4.29'
+                eva: '4.5.0'
             },
             submissionTemplate: {
                 version: 'V1.1.5'
